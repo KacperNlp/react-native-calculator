be579b9c25ebcb8b45bac8afad68b532f5206718
Kacper Nalepa <kacper.nlp@wp.pl>
2022-02-20 21:22:44 +0100

    code refactoring

--- App.js
+++ App.js
@@ -1,14 +1,10 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
-import { CalculatorPanel } from "./components/CalculatorPanel";
-import Header from "./components/Header";
-
 export default function App() {
   return (
     <View style={styles.container}>
-      <Header />
-      <CalculatorPanel />
+      <Text>Hello there</Text>
     </View>
   );
 }
